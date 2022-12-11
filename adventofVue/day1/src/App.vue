761cc8718a09e3926a3cecf7a769511238f7d457
XamzatJR <bahaev.hamzat95@gmail.com>
2022-12-11 14:06:01 +0300

    day1 solution

--- adventofVue/day1/src/App.vue
+++ adventofVue/day1/src/App.vue
@@ -2,8 +2,29 @@
 import { ref, watch } from 'vue'
 
 const searchTerm = ref('')
-
-const findProducts = async term => {}
+const result = ref([])
+const loading = ref(false)
+let timerID = null
+const findProducts = async term => {
+  loading.value = true
+  result.value.products = []
+  clearTimeout(timerID)
+  if (term === '') {
+    loading.value = false
+    return
+  }
+  timerID = setTimeout(async () => {
+    try {
+      result.value = await fetch(`https://dummyjson.com/products/search?q=${term.toLowerCase()}`)
+        .then(res => res.json())
+        .then(data => data)
+      loading.value = false
+    } catch {
+      alert('Scoop didi woop')
+    }
+    loading.value = false
+  }, 300)
+}
 
 watch(searchTerm, newTerm => findProducts(newTerm))
 </script>
@@ -13,7 +34,9 @@ watch(searchTerm, newTerm => findProducts(newTerm))
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
     <ul class="list-disc">
-      <li>Display suggestions here</li>
+      <li v-for="product in result.products">{{ product.title }} - {{ product.price }}$</li>
     </ul>
+    <div v-if="loading">Loading...</div>
+    <div v-if="searchTerm && result.total == 0">Cannot find anything :(</div>
   </div>
 </template>
