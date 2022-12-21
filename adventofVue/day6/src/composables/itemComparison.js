418383d6da3e34b539ed34e290e4becfaaf312c3
XamzatJR <bahaev.hamzat95@gmail.com>
2022-12-21 23:11:08 +0300

    fix: fix the composable

--- adventofVue/day6/src/composables/itemComparison.js
+++ adventofVue/day6/src/composables/itemComparison.js
@@ -2,19 +2,17 @@ import { computed } from 'vue';
 export const useItemComparison = (first, second) => {
   const result = computed(() => {
     if (!first.value || !second.value) {
-      return null;
+      return '';
     }
 
     if (first.value?.id === second.value?.id) {
       return 'These are the same items';
     }
 
-    let x = Math.round(first.value.price / second.value.price);
-    if (first.value.price > second.value.price) {
-      return `You can get ${x}x ${second.value.title} for about the same price as a single ${first.value.title}`;
-    } else {
-      return `You can get ${x}x ${second.value.title} for about the same price as a single ${first.value.title}`;
-    }
+    const [expensive, cheap] = [first.value, second.value].sort((a, b) => b.price - a.price);
+    const x = Math.round(expensive.price / cheap.price);
+
+    return `You can get ${x}x ${cheap.title} for about the same price as a single ${expensive.title}`;
   });
 
   return result;
