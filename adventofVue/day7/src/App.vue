1fad4160fec389ea68bbe405bef8e44f0910880d
XamzatJR <bahaev.hamzat95@gmail.com>
2022-12-22 20:50:53 +0300

    adventOfVue: day7 solution

--- adventofVue/day7/src/App.vue
+++ adventofVue/day7/src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import ChristmasPresent from './components/ChristmasPresent.vue'
 import ChristmasTree from './components/ChristmasTree.vue'
 import { ref } from 'vue'
@@ -7,23 +7,29 @@ const presents = ref(['small-red-gift', 'blue-gift', 'tall-red-gift'])
 const underTheTree = ref([])
 
 const startDrag = (evt, index) => {
-  // TODO
+  evt.dataTransfer.setData('drag-item', index)
+  evt.dataTransfer.effectAllowed = 'move'
 }
 
 const onDrop = evt => {
-  // TODO
+  const id = evt.dataTransfer.getData('drag-item')
+  underTheTree.value.push(presents.value.splice(id, 1)[0])
 }
 </script>
 
 <template>
-  <div class="flex flex-col items-center mt-24 min-h-screen w-full">
-    <h1 class="mt-8 text-xl font-bold">Drag the presents under the tree!</h1>
-    <!-- TODO: make ChristmasTree our drop zone! -->
-    <ChristmasTree :presents="underTheTree" class="mt-16" />
-    <div class="pt-32 mt-32 bg-gray-100 w-full justify-center flex-1">
+  <div class="flex flex-col items-center mt-12 min-h-screen w-full">
+    <h1 class="mt-3 text-xl font-bold">Drag the presents under the tree!</h1>
+    <ChristmasTree @dragover.prevent @drop="onDrop" :presents="underTheTree" class="mt-16" />
+    <div class="pt-16 mt-32 bg-gray-100 w-full justify-center flex-1">
       <div class="flex items-end justify-center" v-auto-animate>
-        <!-- TODO: make each present draggable -->
-        <ChristmasPresent v-for="(p, index) in presents" :key="p" :name="p" />
+        <ChristmasPresent
+          @dragstart="startDrag($event, index)"
+          draggable="true"
+          v-for="(p, index) in presents"
+          :key="p"
+          :name="p"
+        />
       </div>
     </div>
   </div>
