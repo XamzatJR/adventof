f34e1e6b69825584f050a29364c05c307e2c5865
XamzatJR <bahaev.hamzat95@gmail.com>
2023-01-04 00:56:23 +0300

    adventOfVue: day9 solution

--- adventofVue/day9/src/App.vue
+++ adventofVue/day9/src/App.vue
@@ -6,17 +6,41 @@
       </div>
       <div class="mt-2 flex justify-center items-center">
         <img
-          v-for="present in presents"
+          v-for="present in sortedPresents"
           :key="present.id"
           :src="present.src"
           :alt="`Present ${present.id}`"
           data-qa="present"
         />
       </div>
+      <button
+        class="bg-slate-700 hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg flex items-center justify-center mx-auto mt-8"
+        @click="sort"
+      >
+        Toggle sort
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue'
 import presents from './presents.json'
+const sortFunctions = [
+  (a, b) => a.id - b.id,
+  (a, b) => a.dimensions.height + a.dimensions.width > b.dimensions.height + b.dimensions.width,
+  (a, b) => a.dimensions.height + a.dimensions.width < b.dimensions.height + b.dimensions.width,
+]
+
+let sortedFn = ref(0)
+let idx = 0
+function sort() {
+  const length = sortFunctions.length
+  idx++
+  if (idx === length) idx = 0
+  sortedFn.value = idx
+}
+const sortedPresents = computed(() => {
+  return [...presents].sort(sortFunctions[sortedFn.value])
+})
 </script>
