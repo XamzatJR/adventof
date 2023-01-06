a81180d6b461d302b61ba73f941329c9ccd05c82
XamzatJR <bahaev.hamzat95@gmail.com>
2023-01-06 23:40:11 +0300

    adventOfVue: day16 solution

--- adventofVue/day16/src/main.js
+++ adventofVue/day16/src/main.js
@@ -4,8 +4,20 @@ import './base.css'
 
 const app = createApp(App)
 
-app.directive('christmas', (/* todo*/) => {
-  /* todo*/
+app.directive('christmas', (el, { arg, value }) => {
+  el.classList.add('christmas-text')
+  switch (arg) {
+    case 'red':
+      el.classList.add('red')
+      break
+    case 'green':
+      el.classList.add('green')
+      break
+    default:
+      el.classList.add('red-green')
+      if (value) el.style.animationDuration = value + 's'
+      break
+  }
 })
 
 app.mount('#app')
