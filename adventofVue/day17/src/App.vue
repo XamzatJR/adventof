1c50b4aaa2aa5aaf7578139e09fac75bdf499ac3
XamzatJR <bahaev.hamzat95@gmail.com>
2023-01-07 15:37:34 +0300

    adventOfVue: day17 solution

--- adventofVue/day17/src/App.vue
+++ adventofVue/day17/src/App.vue
@@ -3,11 +3,25 @@ import NorthPoleDistance from './components/NorthPoleDistance.vue'
 </script>
 
 <template>
-  <div class="container mx-auto px-4">
+  <div class="container flex flex-col items-center mt-24 px-4">
     <h2>Example 1</h2>
-    <NorthPoleDistance> </NorthPoleDistance>
+    <NorthPoleDistance v-slot="{ unit, distance, toggleUnit }">
+      <div class="text-lg">You're currently: {{ distance }} {{ unit }}s away from the North Pole</div>
+      <button class="text-white mt-4 bg-amber-400 rounded-lg py-2 px-4" @click="toggleUnit">Toggle unit</button>
+    </NorthPoleDistance>
 
     <h2 class="mt-16">Example 2</h2>
-    <NorthPoleDistance> </NorthPoleDistance>
+    <NorthPoleDistance v-slot="{ unit, distance, toggleUnit }">
+      <div class="flex bg-slate-50 gap-3 flex-col items-center border-emerald-400 rounded-lg border-2 px-14 py-8">
+        <div class="">Current distance from the North Pole</div>
+        <div class="">{{ distance }} {{ unit }}s</div>
+        <button
+          class="text-white px-4 py-2 rounded-lg flex justify-center items-center bg-emerald-500 text-base"
+          @click="toggleUnit"
+        >
+          Switch to {{ unit === 'km' ? 'miles' : 'km' }}
+        </button>
+      </div>
+    </NorthPoleDistance>
   </div>
 </template>
--- adventofVue/day17/src/components/NorthPoleDistance.vue
+++ adventofVue/day17/src/components/NorthPoleDistance.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
-import { getDistanceKm, getDistanceMiles } from '@/utils/distance'
+import { getDistanceKm, getDistanceMiles } from '../utils/distance'
 import { useGeolocation } from '@vueuse/core'
 import { ref, computed } from 'vue'
 
 // hint: coords.latitude + cords.latitude
 const { coords } = useGeolocation()
+console.log(coords.value, 'Value')
+console.log(coords.value.latitude, 'Latitude')
 
 const unit = ref<'km' | 'mile'>('mile')
-
-const toggleUnit = () => {}
+const distance = computed(() => {
+  if (unit.value === 'mile') {
+    return getDistanceMiles(coords.value.latitude, coords.value.longitude)
+  } else {
+    return getDistanceKm(coords.value.latitude, coords.value.longitude)
+  }
+})
+const toggleUnit = () => {
+  unit.value === 'mile' ? (unit.value = 'km') : (unit.value = 'mile')
+}
 </script>
 
 <template>
   <!-- this should only render a slot -->
-  <slot />
+  <slot :toggleUnit="toggleUnit" :distance="distance" :unit="unit" />
 </template>
